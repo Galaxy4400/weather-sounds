aaa15f70ad6cc68c37eca8d7fae39e07c03439e1
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-12-19 01:01:06 +0300

    init

---
+++ webpack.config.common.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: './scripts/index.ts',
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: path.resolve(__dirname, 'public/assets'), to: path.resolve(__dirname, 'dist/assets') },
+			],
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", {
+					loader: "postcss-loader",
+					options: {
+						postcssOptions: {
+							plugins: [
+								[
+									"postcss-preset-env",
+								],
+							],
+						}
+					}
+				}, "sass-loader"],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.[tj]sx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+}
