47015dbdaf60b976c979d816d7690caa871a332e
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-12-19 14:13:29 +0300

    feat: прикрутил логику

---
+++ src/scripts/main.ts
@@ -0,0 +1,37 @@
+const processAudio = (item: HTMLElement) => {
+	const audio = item.querySelector('.item__audio') as HTMLAudioElement;
+		
+	if (item.classList.contains('active')) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
+}
+
+const disactiveOthers = (currentItem: HTMLElement) => {
+	(document.querySelectorAll('.item') as NodeListOf<HTMLElement>).forEach((item) => {
+		if (currentItem === item) return;
+
+		item.classList.remove('active');
+		document.querySelector(`[data-bg="${item.dataset.no}"]`).classList.remove('active');
+
+		processAudio(item);
+	});
+};
+
+const itemClick = (item: HTMLElement) => {
+	item.classList.toggle('active');
+	document.querySelector(`[data-bg="${item.dataset.no}"]`).classList.toggle('active');
+
+	processAudio(item);
+
+	disactiveOthers(item);
+};
+
+export const initialize = () => {
+	document.querySelector('.items').addEventListener('click', (e) => {
+		const item = (e.target as HTMLElement).closest('.item') as HTMLElement;
+
+		if (item) itemClick(item);
+	});
+};
